8fba3197924e7c19aacf28a6cd0a508c5a55c13f
syassine <yassine.sghaier@ynov.com>
2023-11-20 10:11:00 +0100

    ascii-art

--- main/main.go
+++ main/main.go
@@ -73,67 +73,73 @@ func motAuHasardDansFichier(nomFichier string) (string, error) {
 	return motAuHasard, nil
 }
 
+
 func guess(word string) {
-	tab_hangman()
-	a := len(word)
-	var word_array []string
-	for i := 0; i < a; i++ {
-		word_array = append(word_array, "_")
-	}
-	if len(os.Args) != 2 {
-		fmt.Println("Utilisation : ./main.go <nom_fichier>")
-		os.Exit(1)
-	}
-	fmt.Println("Good luck, you have 10 attempts to find the word !!!")
-	essaiRestant := 10
-	var lettersGuessed []string
-	for essaiRestant > 0 {
-		for i := 0; i < len(word_array); i++ {
-			fmt.Print(word_array[i])
-			fmt.Print(" ")
-		}
+    tab_hangman()
+    a := len(word)
+    var word_array []string
+    for i := 0; i < a; i++ {
+        word_array = append(word_array, "_")
+    }
+	b:=os.Args
+    if len(os.Args) != 4 && b[2]!="--letterFile"{
+    	fmt.Println("Usage: ./hangman <words_file> --letterFile <ascii_file>")
+    	os.Exit(1)
+    }
+    fmt.Println("Good luck, you have 10 attempts to find the word !!!")
+    essaiRestant := 10
+    var lettersGuessed []string
+    correctGuess := false
+	Print_Ascii(word_array)
+    for essaiRestant > 0 {
+        var guess string
+        fmt.Print("\nGuess : ")
+        fmt.Scanln(&guess)
 
-		var guess string
-		fmt.Print("\nGuess (letter or word): ")
-		fmt.Scanln(&guess)
+        if len(guess) == 1 {
+            letterGuessed := false
+            if !in(lettersGuessed, guess) {
+                lettersGuessed = append(lettersGuessed, guess)
 
-		if len(guess) == 1 {
-			letterGuessed := false
-			if !in(lettersGuessed, guess) {
-				lettersGuessed = append(lettersGuessed, guess)
+                for index, char := range word {
+                    if string(char) == guess {
+                        word_array[index] = guess
+                        letterGuessed = true
+                    }
+                }
 
-				for index, char := range word {
-					if string(char) == guess {
-						word_array[index] = guess
-						letterGuessed = true
-					}
-				}
+                if !letterGuessed {
+                    essaiRestant--
+                    pose_hangman(10 - essaiRestant)
+                } else {
+					Print_Ascii(word_array)
+                }
+            } else {
+                fmt.Println("You already guessed that letter. Try a different one.")
+            }
+        } else if len(guess) >= 2 {
+            if guess == word {
+                for index := range word {
+                    word_array[index] = guess
+                }
+                correctGuess = true
+				} else {
+                essaiRestant -= 2
+                pose_hangman(10 - essaiRestant)
+            }
+        }
 
-				if !letterGuessed {
-					essaiRestant--
-					pose_hangman(10 - essaiRestant)
-				}
-			} else {
-				fmt.Println("You already guessed that letter. Try a different one.")
-			}
-		} else if len(guess) >= 2 {
-			if guess == word {
-				fmt.Println("Congratulations! You guessed the word:", word)
-				os.Exit(0)
-			} else {
-				essaiRestant -= 2
-				pose_hangman(10 - essaiRestant)
-			}
-		}
+        if strings.Join(word_array, "") == word || correctGuess {
+            Print_Ascii([]string{word})
+            fmt.Println("Congratulations! You guessed the word:", word)
+            os.Exit(0)
+        }
+    }
 
-		if strings.Join(word_array, "") == word {
-			fmt.Println("Congratulations! You guessed the word:", word)
-			os.Exit(0)
-		}
-	}
-
-	fmt.Println("You've exhausted all your attempts. The word was:", word)
+    fmt.Println("You've exhausted all your attempts. The word was:", word)
 }
+
+
 
 func in(slice []string, val string) bool {
 	for _, item := range slice {
@@ -147,7 +153,8 @@ func in(slice []string, val string) bool {
 var lines2 []string
 
 func tab_ascii() {
-	originalFile, err := os.Open("standard.txt")
+	b:=os.Args[3]
+	originalFile, err := os.Open(b)
 	if err != nil {
 		fmt.Println("Erreur lors de l'ouverture du fichier d'origine :", err)
 		return
@@ -165,3 +172,25 @@ func tab_ascii() {
 	}
 
 }
+func pose_ascii(nb rune) {
+	const groupSize = 9
+	start := (nb - 1) * groupSize
+	end := nb * groupSize
+	for i := start; i < end; i++ {
+		fmt.Println(lines2[i])
+	}
+}
+
+func Print_Ascii(array []string) {
+    tab_ascii()
+
+    var result strings.Builder
+    for _, str := range array {
+        result.WriteString(str)
+    }
+
+    for _, r := range result.String() {
+        pose_ascii(rune(r) - 31)
+    }
+}
+
